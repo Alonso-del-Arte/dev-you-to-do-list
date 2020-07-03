e7dadf21253c04237d34d70f20792af15b1166c5
Alonso del Arte <alonso.delarte@gmail.com>
2020-07-03 12:44:33 -0400

    Serialization prep

--- src/todolist/Activity.java
+++ src/todolist/Activity.java
@@ -1,8 +1,11 @@
 package todolist;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class Activity {
+public class Activity implements Serializable {
+
+    private static final long serialVersionUID = 4549631111164082995L;
 
     public static void placeHold() {
         //
---
+++ src/todolist/ToDoList.java
@@ -0,0 +1,11 @@
+package todolist;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ToDoList implements Serializable {
+
+    private static final long serialVersionUID = 4549634654512102195L;
+
+}
